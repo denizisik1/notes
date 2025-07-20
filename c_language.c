d1e5fcb355d8aa580a0e46a77fa6302a448fb68c
denizisik <denizkisisel1@gmail.com>
2025-07-20 16:38:57 +0300

    initial commit

---
+++ c_language.c
@@ -0,0 +1,14 @@
+// education link:
+// portfolio courses c language course: https://www.youtube.com/watch?v=ssJY5MDLjlo
+
+// linking:
+// gcc -o main main.c -lpthread -lmysqlclient
+
+// safer strcpy_alternative
+int snprintf(char *str, size_t size, const char *format, ...);
+
+// brief(easy to remember) explanation of pointers etcetera
+<var_type> *<var_name>: pointer type
+*<var_name>: access
+&var_name: address
+var_name: value
